cf703a748c37690335fa8a66182aad5e133101be
Abhinav Tiwari <abhi.kay.tiwari@gmail.com>
2019-05-06 19:14:42 -0600

    push for heroku

--- server.js
+++ server.js
@@ -3,18 +3,23 @@ const app = express();
 const mongoose = require('mongoose');
 const expressJwt = require('express-jwt');
 require("dotenv").config();
-const PORT = 6669;
+const PORT = process.env.PORT || 6669;
+const path = require("path");
 
 app.use(express.json());
 app.use("/auth", require("./routes/auth"));
 app.use("/api/scores", require("./routes/scores"));
+app.use(express.static(path.join(__dirname, "client", "build")))
 
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 app.listen(PORT, () => {
     console.log( `(x) ${PORT} running`);
 })
 
 mongoose.set('useCreateIndex', true);
-mongoose.connect("mongodb://localhost:27017/scores",
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/scores",
     { useNewUrlParser: true },
     (err) => {
         if (err) throw err;
